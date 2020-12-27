cc01d556e37291bd56aacfbef1112a5ee5573370
YahyaM <Adamali5555555555@gmail.com>
2020-12-27 22:30:18 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// write your code 
+app.get('/user',(req,res)=>{
+    res.json({massge: "Welcome ", name:req.query.name});
+})
+
+
+app.post('/user/yahya',(req,res)=>{
+    res.json({ message:req.body.massage ,name:req.body.name });
+    
+})
+app.get('/user/yahya',(req,res)=>{
+    res.json({massge: "Welcome"});
+})
+app.listen(3000,()=>{
+    console.log('server running portNo.3000'); 
+ });
